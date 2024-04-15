0bc153b5e68358fab430dea07083b5dea3072b70
Gregor <gregor.brett@theconfigteam.co.uk>
2024-04-16 00:19:53 +0100

    build stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/5cc8f82c-a3a0-4dbc-aa58-d6661bc2667e-mvncrb.png",
+  "https://utfs.io/f/5cc8f82c-a3a0-4dbc-aa58-d6661bc2667e-mvncrb.png",
+  "https://utfs.io/f/5cc8f82c-a3a0-4dbc-aa58-d6661bc2667e-mvncrb.png",
+  "https://utfs.io/f/5cc8f82c-a3a0-4dbc-aa58-d6661bc2667e-mvncrb.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1 className="text-4xl font-bold">Gallery in progress</h1>
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} className="h-40 w-full object-cover" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }

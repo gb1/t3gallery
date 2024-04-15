af75525f932ccefb939b56203a38400ed7fb131b
Gregor <gregor.brett@theconfigteam.co.uk>
2024-04-16 00:43:00 +0100

    db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/5cc8f82c-a3a0-4dbc-aa58-d6661bc2667e-mvncrb.png",
@@ -12,12 +13,21 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            <p>{post.name}</p>
+          </div>
+        ))}
+
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={index + 1} className="w-48">
             <img src={image.url} className="h-40 w-full object-cover" />
           </div>
         ))}
